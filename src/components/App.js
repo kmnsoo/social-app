870062f5f82cc0c3c0d23c8c6c40c7a517a7dad4
kmnsoo <wkdska22@naver.com>
2021-11-11 17:31:48 +0900

    add AuthStateChange to Login and sweep page

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { auth } from "../fbase";
 import AppRouter from "./Router";
 
 function App() {
-  // console.log(auth.currentUser);
+  const [init, setInit] = useState(false);
+  //
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      // init이 fasle면 아래 리턴의 라우터를 숨긴다. 즉, 로그인이 되면 AppRouter작동(이동)
+      setInit(true);
+    });
+  }, []);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
-  console.log(auth.currentUser);
-  // setInterval(() => {
-  //   console.log(auth.currentUser);
-  // }, 2000);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()}Nwitter </footer>
     </>
   );
