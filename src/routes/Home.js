5a182a2c2b4567dd226013f2139e3707a508d83e
kmnsoo <wkdska22@naver.com>
2021-11-15 14:49:41 +0900

    Home screen에서 tweet기능 추가중

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Home() {
-  return <span>Home</span>;
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onsubmit}>
+        <input value={nweet} onChange={onChange} type="text" placeholder="What`s on your mind?" maxLength={120} />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
