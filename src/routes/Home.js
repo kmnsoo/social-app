2c081e3f0eea84640c2d3d8c5a89be69f5311aeb
kmnsoo <wkdska22@naver.com>
2021-11-18 21:02:08 +0900

    트윗 기능 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,22 @@
+import { dbService } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 function Home() {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID:", docRef.id);
+    } catch (error) {
+      console.log("Error adding document:", error);
+    }
+
+    setNweet("");
   };
   const onChange = (event) => {
     const {
@@ -13,8 +26,14 @@ function Home() {
   };
   return (
     <div>
-      <form onSubmit={onsubmit}>
-        <input value={nweet} onChange={onChange} type="text" placeholder="What`s on your mind?" maxLength={120} />
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What`s on your mind?"
+          maxLength={120}
+        />
         <input type="submit" value="Nweet" />
       </form>
     </div>
